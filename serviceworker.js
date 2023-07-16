f4294445735e26d4676e7d2c09823fd52c187562
jeremy england <codeartery@users.noreply.github.com>
2023-07-16 14:43:32 -0500

    Add service worker
    Support for offline mode

---
+++ serviceworker.js
@@ -0,0 +1,37 @@
+
+const cacheName = 'pwa-assets'
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                '/', 
+                '/manifest.json',
+                '/index.html',                
+                '/pages/edit.html',
+                '/pages/history.html',
+                '/pages/settings.html',
+                '/styles/global.css', 
+                '/styles/index.css',
+                '/styles/edit.css',
+                '/styles/history.css'
+            ])
+        })
+    )
+    console.log('service worker installed')
+})
+
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cacheResponse => {
+            const networkResponse = fetch(event.request).then(response => {
+                caches.open(cacheName).then(cache => {
+                    cache.put(event.request, response.clone())
+                })
+                return response
+            })
+            return networkResponse || cacheResponse
+        })
+    )
+})
