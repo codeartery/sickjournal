115328698098b3dc566454d4020a715484621955
jeremy england <codeartery@users.noreply.github.com>
2023-09-16 12:14:16 -0500

    Fix onupgradeneeded
    IndexedDB onupgradeneeded was using the wrong transaction to make
    cahnges

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
-const dbConnection = window.indexedDB.open('sickjournaldb', 3);
+const dbConnection = window.indexedDB.open('sickjournaldb', 4);
 var dbCurrentEvent = null
 var dbCurrentEventEntryIndex = -1
 
 dbConnection.onupgradeneeded = (event) => {
     const db = event.target.result;            
+    const tr = event.target.transaction;
     
     console.log(`Upgrading from version ${event.oldVersion} to version ${db.version}`)
     //alert(`Upgrading from version '${event.oldVersion}' to version '${db.version}'.`)
@@ -15,9 +16,9 @@ dbConnection.onupgradeneeded = (event) => {
         const store2 = db.createObjectStore('sickevent', {keyPath: 'id', autoIncrement: true})
         console.log('Successfully created store', store2)
     }
-    if (event.oldVersion < 3) {        
-        const tr3 = db.transaction(db.objectStoreNames, 'readwrite')
-        const store3 = tr3.objectStore('sickevent')
+    if (event.oldVersion < 4) {        
+        
+        const store3 = tr.objectStore('sickevent')
         
         //notes added
         store3.getAll().onsuccess = (event3) => {
@@ -27,5 +28,6 @@ dbConnection.onupgradeneeded = (event) => {
             })
             console.log(`Successfully upgraded to ${db.version}`)
         }
-    }    
+    }
+    
 }
