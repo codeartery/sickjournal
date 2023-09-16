f07a46f7136000e6222aaa53726b239d9c87e3b5
jeremy england <codeartery@users.noreply.github.com>
2023-09-16 12:23:22 -0500

    Updating internal code comments

--- database.js
+++ database.js
@@ -7,20 +7,21 @@ dbConnection.onupgradeneeded = (event) => {
     const tr = event.target.transaction;
     
     console.log(`Upgrading from version ${event.oldVersion} to version ${db.version}`)
-    //alert(`Upgrading from version '${event.oldVersion}' to version '${db.version}'.`)
     
     if (event.oldVersion < 1) {
-        //version one was for testing and was never in production
+        //version 1: beta testing
     }    
     if (event.oldVersion < 2) {
+        //version 2: initial release        
         const store2 = db.createObjectStore('sickevent', {keyPath: 'id', autoIncrement: true})
         console.log('Successfully created store', store2)
     }
-    if (event.oldVersion < 4) {        
-        
+    if (event.oldVersion < 3) {
+        //version 3: figuring out upgrade procedure
+    }
+    if (event.oldVersion < 4) {
+        //version 4: journal feature added        
         const store3 = tr.objectStore('sickevent')
-        
-        //notes added
         store3.getAll().onsuccess = (event3) => {
             event3.target.result.forEach(item3 => {
                 item3.journal = ""
@@ -28,6 +29,5 @@ dbConnection.onupgradeneeded = (event) => {
             })
             console.log(`Successfully upgraded to ${db.version}`)
         }
-    }
-    
+    }    
 }
